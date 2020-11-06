bf14c095d1d44936e63ea330d8032ea74a175d24
adam <mr.adamtyler@gmail.com>
2020-11-06 23:44:23 +0000

    Refactor to include Account class; withdrawal and deposit to receive account object as input

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-let balance = 500.00;
+// let balance = 500.00;
+
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
+
 
 class Withdrawal {
-
-  constructor(amount) {
+  constructor(amount, account) {
+    this.account = account;
     this.amount = amount;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
+  }
+
+}
+
+
+class Deposit {
+  constructor(amount, account) {
+    this.account = account;
+    this.amount = amount;
+  }
+
+  commit() {
+    this.account.balance += this.amount;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("sillyBilly");
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
